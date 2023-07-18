3f52022e02ccffc52e491e88de0720e0fd8de3e2
Yigit Polat <yigitpolatt@gmail.com>
2023-07-18 02:01:48 +0200

    fixes

--- coctail-machine/fill_level_detection.py
+++ coctail-machine/fill_level_detection.py
@@ -9,12 +9,15 @@ import imutils
 bottle_image = cv2.imread("../images/sample_bottle.png")
 bottle_gray = cv2.split(bottle_image)[0]
 
+# blur image
+bottle_gray = cv2.GaussianBlur(bottle_gray, (7, 7), 0)
+
 # manual threshold
 # plt.hist(bottle_gray.ravel(), 256,[0, 256]); plt.show()
 (T, bottle_threshold) = cv2.threshold(bottle_gray, 27.5, 255, cv2.THRESH_BINARY_INV)
 # cv2.imshow("Bottle Gray Threshold 27.5", bottle_threshold)
 
-# apply opening operation
+# apply opening operation to remove the white edges of the water
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
 bottle_open = cv2.morphologyEx(bottle_threshold, cv2.MORPH_OPEN, kernel)
 # cv2.imshow("Bottle Open 5 x 5", bottle_open)
@@ -23,7 +26,7 @@ bottle_open = cv2.morphologyEx(bottle_threshold, cv2.MORPH_OPEN, kernel)
 contours = cv2.findContours(bottle_open.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 contours = imutils.grab_contours(contours)
 bottle_original_image = bottle_image.copy()
-cv2.drawContours(bottle_original_image, contours, -1, (255, 0, 0), 2)
+# cv2.drawContours(bottle_original_image, contours, -1, (255, 0, 0), 2)
 # cv2.imshow("All Contours", bottle_original_image)
 
 # sort contours by area
